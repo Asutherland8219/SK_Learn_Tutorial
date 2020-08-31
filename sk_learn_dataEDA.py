d7d3b48fd1b06eef41f5d797174d71f391b4ba78
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2020-08-31 15:28:40 -0300

    Changed interpreter and started EDA

--- sk_learn_dataEDA.py
+++ sk_learn_dataEDA.py
@@ -1,9 +1,22 @@
 ### Machine learning tutorial on Sci Kit learn from Data camp; tutorial can be found here : https://www.datacamp.com/community/tutorials/machine-learning-python ###
 from sklearn import datasets
+import pandas as pd
 
 # load the data 
-digits = dataets.load_digits()
+digits = datasets.load_digits()
 
-# Display the data head 
-print(digits.head())
+# Display the data set 
+print(digits)
+
+# Explore the data set 
+print(digits.keys())
+
+# Print data 
+print(digits.data)
+
+# Print out the target values
+print(digits.target)
+
+# Print ouf the description
+print(digits.DESCR)
 
