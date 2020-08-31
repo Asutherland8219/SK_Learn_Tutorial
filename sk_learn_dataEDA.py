0211a999b6274e08dd33c633bef2df059139e2b1
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2020-08-31 12:15:30 -0300

    New tutorial

---
+++ sk_learn_dataEDA.py
@@ -0,0 +1,9 @@
+### Machine learning tutorial on Sci Kit learn from Data camp; tutorial can be found here : https://www.datacamp.com/community/tutorials/machine-learning-python ###
+from sklearn import datasets
+
+# load the data 
+digits = dataets.load_digits()
+
+# Display the data head 
+print(digits.head())
+
