7dde8887425478f78a4d616ae9be652c84fa45fa
Asutherland8219 <60415557+Asutherland8219@users.noreply.github.com>
2020-09-01 21:03:10 -0300

    Create Principal_Component_Analysis.py

---
+++ Principal_Component_Analysis.py
@@ -0,0 +1,4 @@
+### Dimension reduction techniques such as principal component analysis (PCA) are use dt ohelp reduce the number of dimensions thus creating a better model. Models with less dimensions (2-3) are easier to be visualized. The idea behind PCA is to find the variables that contain the most information. ###
+## Aka it creates a linear combination of 2 variables that contains the most information. It yiels the directions that maximize the variance of the data. ##
+
+# Create a Randomized PCA model that takes 2 components #
